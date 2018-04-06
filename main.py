a3b73a61e4bfe2aa3c9fe81d3c63b09fc1acfb67
Biowulf513 <cherepanov92@gmail.com>
2018-04-06 13:04:42 +0500

    1.2 Проверка строки на наличие message_id

--- main.py
+++ main.py
@@ -26,16 +26,23 @@ __email__ = "cherepanov92@gmail.com"
 3. Результат обработки представить в виде таблицы или отчета. 
     Сохраним конечные данные используя CSV
 '''
+import re
 
 class LogParser():
+    reg_exp = {'message_id':r':\s+([0-9A-Z]{11})\W'}
+
     def __init__(self, filename):
         self.filename = filename
 
     def read_log(self):
         with open(self.filename, mode='r') as f:
             for line in f:
-                print(line)
+                message_id = re.findall(self.reg_exp['message_id'], line)
+                if message_id:
+                    self.log_line_analise(message_id, line)
 
+    def log_line_analise(self, message_id, line):
+        pass
 
 if __name__ == '__main__':
     parse = LogParser('maillog')
